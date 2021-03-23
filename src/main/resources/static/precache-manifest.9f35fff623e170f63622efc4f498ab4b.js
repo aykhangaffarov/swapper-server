3510dd2940f2d5dde7cac30a6fb2bffa650c1eca
aykhangaffarov <ayxan.qaffarov@gmail.com>
2021-03-23 17:23:36 +0100

    With front-end

---
+++ src/main/resources/static/precache-manifest.9f35fff623e170f63622efc4f498ab4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dd3419575e6ce76c622818726bf01e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aac16e5ebf293f92628",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c19549fc633857b7874b",
+    "url": "/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "4aac16e5ebf293f92628",
+    "url": "/static/js/2.9a03ba3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a03ba3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19549fc633857b7874b",
+    "url": "/static/js/main.8623e9fb.chunk.js"
+  },
+  {
+    "revision": "9797c13a4a02f0effb24",
+    "url": "/static/js/runtime-main.4bcacc0f.js"
+  }
+]);
